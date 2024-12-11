c58424809af074732a2790863a0d0948705cda2b
MarioUbeira <mario@ubeira.es>
2024-12-11 21:21:30 +0100

    refactor(src): :sparkles: Crear clase para o Axente predictor e darlle un esqueleto base

--- src/agent.py
+++ src/agent.py
@@ -1,9 +1,21 @@
 from rules import GameAction
 import random
 
-def get_computer_action():
-    computer_selection = random.randint(0, len(GameAction) - 1)
-    computer_action = GameAction(computer_selection)
-    print(f"Computer picked {computer_action.name}.")
+class PredictAgent:
+    def __init__(self):
+        self.user_moves = []
 
-    return computer_action
+    def last_matches(self, move):
+        """
+        Rexistra o movemento do usuario.
+        """
+        self.user_moves.append(move)
+
+    def predict(self):
+        """
+        Predí o seguinte movemento do usuario baseado no historial de partidas (last_matches) cando
+        este teña un mínimo de 10 partidas rexistradas, mentres non as teña actuará aleatoriamente. 
+        """
+        if len(self.user_moves) < 10:
+            return random.choice(list(GameAction))
+        return random.choice(list(GameAction)) #Aquí ira a "intelixencia" do axente
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from rules import GameAction, assess_game
-from agent import get_computer_action
+from agent import PredictAgent
 from user import get_user_action, play_another_round
 
 def main():
-
+    predictin = PredictAgent()
+    
     while True:
         try:
             user_action = get_user_action()
@@ -11,8 +12,9 @@ def main():
             range_str = f"[0, {len(GameAction) - 1}]"
             print(f"Invalid selection. Pick a choice in range {range_str}!")
             continue
-
-        computer_action = get_computer_action()
+        
+        predictin.last_matches(user_action)
+        computer_action = predictin.predict()
         assess_game(user_action, computer_action)
 
         if not play_another_round():
