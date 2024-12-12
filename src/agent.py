5ba4914e02eb82e395d3a91f983ac193ddfc2e5e
MarioUbeira <mario@ubeira.es>
2024-12-12 17:16:03 +0100

    fix(Agent): :bug: Arranxar lóxica que seguía o axente en caso de non atopar patróns

--- src/agent.py
+++ src/agent.py
@@ -40,28 +40,24 @@ class PredictAgent:
 
     def predict(self):
         """
-        Predí o seguinte movemento do usuario baseado no historial de partidas (last_matches) cando
-        este teña un mínimo de 10 partidas rexistradas, mentres non as teña actuará aleatoriamente. 
+        Predí o seguinte movemento do usuario baseado no historial de partidas (last_matches), cando
+        non atopa este patrón actúa en función do resultado da última partida. 
         """
         if len(self.user_moves) == 0:
             return random.choice(list(GameAction))
         
-        ted_kaczynski = self.pattern_detecter() # Ted Kaczynski e o meu patrón
+        patron = self.pattern_detecter()
         
-        if ted_kaczynski:
+        if patron:
             print("Patrón detectado")
-        elif self.last_match == GameResult.Victory:
+            
+        if self.last_match == GameResult.Victory:
             last_move = self.user_moves[-1]
-            next_move = Victories[last_move][0]
+            next_move = Defeats[Defeats[last_move][0]][0]
         elif self.last_match == GameResult.Defeat:
             last_move = self.user_moves[-1]
-            if last_move == GameAction.Rock:
-                next_move = GameAction.Scissors
-            elif last_move == GameAction.Paper:
-                next_move = GameAction.Rock
-            else:
-                next_move = GameAction.Paper 
+            next_move = Victories[last_move][0]
         else:
             next_move = random.choice(list(GameAction))
+
         return next_move
-            
